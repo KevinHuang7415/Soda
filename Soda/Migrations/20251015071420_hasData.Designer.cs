05904e36c81ab3137335534a7e6cc5366d2b47f0
sfy25700000 <sfy25700000@gmail.com>
2025-10-15 15:20:09 +0800

    增加執行後新增預設資料功能;更新訂單管理功能

--- Soda/Migrations/20251015071420_hasData.Designer.cs
+++ Soda/Migrations/20251015071420_hasData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Soda.Data;
 
@@ -11,9 +12,11 @@ using Soda.Data;
 namespace Soda.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015071420_hasData")]
+    partial class hasData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
