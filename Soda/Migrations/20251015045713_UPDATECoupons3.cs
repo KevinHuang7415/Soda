a1eb8b9b81135bccb4d84736232412520a1e672a
sfy25700000 <sfy25700000@gmail.com>
2025-10-15 12:59:07 +0800

    更新Coupons資料表;完成優惠管理功能

---
+++ Soda/Migrations/20251015045713_UPDATECoupons3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Soda.Migrations
+{
+    /// <inheritdoc />
+    public partial class UPDATECoupons3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
