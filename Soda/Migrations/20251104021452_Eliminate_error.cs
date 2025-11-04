f25f703c929c01c03833e2c88cfc182e76425a06
WHNB-W11-C0364\vicky <sfy25700000@gmail.com>
2025-11-04 10:18:12 +0800

    更新migration

---
+++ Soda/Migrations/20251104021452_Eliminate_error.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Soda.Migrations
+{
+    /// <inheritdoc />
+    public partial class Eliminate_error : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderItems",
+                schema: "dbo",
+                table: "Orders",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderItems",
+                schema: "dbo",
+                table: "Orders",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4000)",
+                oldMaxLength: 4000);
+        }
+    }
+}
